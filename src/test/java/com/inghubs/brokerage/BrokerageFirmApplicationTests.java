5b45db37d28d88fb12803d2e06442fd019af0533
dogukanozdemir <dogu757@gmail.com>
2025-03-11 00:05:17 +0300

    Initial commit 2.0

---
+++ src/test/java/com/inghubs/brokerage/BrokerageFirmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inghubs.brokerage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerageFirmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
